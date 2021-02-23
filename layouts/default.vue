7ef9c65e378899f2d3ea211c6fae4fb77457eeb6
cecimerelo <ceci.merelo@gmail.com>
2021-02-23 18:38:08 +0100

    Milestone: Menu appears lined up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,32 @@
 <template>
-  <div id="app">
+  <div id="app" class="app">
     <v-app>
-      <v-toolbar
-        flat
-        prominent
-      >
-        <a id="logo" :href="baseURL">
-          <img
-            src="../static/logo.jpg"
-            alt="logo"
-            width="18%"
-            height="auto"
-          >
-        </a>
-      </v-toolbar>
+      <header id="link-logo">
+        <div id="container">
+          <div class="right" />
+          <div id="logo">
+            <a :href="baseURL">
+              <img
+                src="../static/logo.png"
+                alt="logo"
+                width="18%"
+                height="auto"
+              >
+            </a>
+          </div>
+            <ul id="links">
+              <router-link
+                v-for="routes in links"
+                :key="routes.id"
+                :to="`${routes.page}`"
+                class="spacing"
+              >
+                {{ routes.text }}
+              </router-link>
+            </ul>
+          <div class="right"/>
+        </div>
+      </header>
     </v-app>
   </div>
 </template>
@@ -22,7 +35,46 @@
 export default {
   data () {
     return {
-      baseURL: undefined
+      baseURL: undefined,
+      links: [
+        {
+          id: 1,
+          text: 'Inicio',
+          page: '/Inicio'
+
+        },
+        {
+          id: 2,
+          text: 'Quienes Somos',
+          page: '/QuienesSomos'
+        },
+        {
+          id: 3,
+          text: 'Servicios',
+          page: '/Servicios',
+          dropdown: ['Club de Ocio', 'Programas', 'Inserción Laboral', 'Servicio Fisioterapia', 'Servicios Sociosanitarios']
+        },
+        {
+          id: 4,
+          text: 'Colabora',
+          page: '/Colabora'
+        },
+        {
+          id: 5,
+          text: 'Voluntariado',
+          page: '/Voluntariado'
+        },
+        {
+          id: 6,
+          text: 'Galería',
+          page: '/Galeria'
+        },
+        {
+          id: 7,
+          text: 'Contacto',
+          page: '/Contacto'
+        }
+      ]
     }
   },
 
@@ -31,3 +83,61 @@ export default {
   }
 }
 </script>
+
+<style>
+
+.spacing {
+  margin-right: 20px;
+  text-decoration: none;
+  padding: 0 10px;
+}
+
+link-logo {
+  display: flex;
+  align-items: center;
+}
+
+#links div {
+  float: left;
+}
+
+#logo img {
+  z-index: -1;
+  width: 250px;
+}
+
+#container {
+  display: flex;
+  justify-content: space-between;
+}
+
+#container ul {
+  margin-block-start: 1em;
+  vertical-align: middle;
+  margin: 40px 100px;
+  list-style: none;
+  line-height: 1;
+}
+
+.right {
+  margin: 50px;
+}
+
+#links {
+  padding: 20px 20px 20px;
+}
+
+#link-logo a {
+  color: #5d5d5d;
+  font-size: x-large;
+}
+
+#link-logo a:hover {
+  color: #000000;
+}
+
+#link-logo img {
+  Padding: 20px 10px 10px;
+}
+
+</style>
